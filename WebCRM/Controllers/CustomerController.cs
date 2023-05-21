f703ed21109d48f7c4e76fcddf6c7ccea3bf5637
Мартин Симонян <simonyan.martin@rambler.ru>
2023-05-21 21:47:49 +0300

    Added a page for the Customer controller with the ability to display database data as a table

---
+++ WebCRM/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using WebCRM.Data;
+using WebCRM.Models;
+
+namespace WebCRM.Controllers
+{
+    public class CustomerController:Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Customer> customers = _context.Customers.ToList();
+            return View(customers);
+
+        }
+    }
+}
