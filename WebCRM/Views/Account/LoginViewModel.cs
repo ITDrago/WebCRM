7c7f3dfadf9daeb5e32c54571095ff2ef59571dc
Мартин Симонян <simonyan.martin@rambler.ru>
2023-06-12 18:33:30 +0300

    Implemented login process and page

---
+++ WebCRM/Views/Account/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCRM.Views.Account
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
