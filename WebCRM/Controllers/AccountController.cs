6258d25e023338400431589a464f0a2ca97f9300
Мартин Симонян <simonyan.martin@rambler.ru>
2023-06-22 20:44:31 +0300

    Registration sketch implemented

--- WebCRM/Controllers/AccountController.cs
+++ WebCRM/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ namespace WebCRM.Controllers
         private readonly ApplicationDbContext _context;
 
 
-        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,ApplicationDbContext context)
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
         {
             _context = context;
             _signInManager = signInManager;
@@ -53,5 +53,41 @@ namespace WebCRM.Controllers
             TempData["Error"] = "Wrong credentials. Please try again";
             return View(loginViewModel);
         }
-    }
+
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                TempData["Error"] = "This email addresses is already in use";
+                return View(registerViewModel);
+            }
+            var newUser = new AppUser()
+            {
+                Email  = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            return RedirectToAction("Index", "Customer");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }    
 }
---
+++ WebCRM/Views/Account/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCRM.Views.Account
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
